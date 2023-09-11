024fb1740f786cc275fd11ae0f003019e48723ad
p-Lukas <16989928+p-Lukas@users.noreply.github.com>
2023-09-11 22:56:13 +0200

    Initial Commit

---
+++ takahe/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  takahe
+//
+//  Created by enai on 11.09.23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
