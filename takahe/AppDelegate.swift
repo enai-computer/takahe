8d81ddfb6ab36f072d449dc3596975abbdd9f0a1
p-Lukas <16989928+p-Lukas@users.noreply.github.com>
2023-09-17 00:58:27 +0200

    make search visible nest

--- takahe/AppDelegate.swift
+++ takahe/AppDelegate.swift
@@ -5,11 +5,24 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    
+    var window: NSWindow!
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        // Create the SwiftUI view that provides the window contents.
+
+        window = NSWindow(
+            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
+            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+            backing: .buffered, defer: false)
+        window.makeKeyAndOrderFront(self)
+        window.isReleasedWhenClosed = false
+        window.center()
+        window.setFrameAutosaveName("Main Window")
+        
+        let mainScreen: NSScreen = NSScreen.screens[0]
+        window.contentView?.enterFullScreenMode(mainScreen)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
--- takahe/ViewController.swift
+++ takahe/ViewController.swift
@@ -1,9 +1,19 @@
 //Created on 11.09.23
 
 import Cocoa
+import WebKit
 
 class ViewController: NSViewController {
 
+    @IBAction func mainSearch(_ searchTerm: NSSearchField) {
+        //TODO: open webview with google search
+        let wv = WKWebView()
+//        let urlString: String =
+//        let url_ = URL(string: "https://www.google.com/search?q=" + searchTerm.stringValue)
+        let urlReq = URLRequest(url: URL(string: "https://www.google.com/search?q=" + searchTerm.stringValue)!)
+        wv.load(urlReq)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
