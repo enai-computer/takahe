03d5bf715b3a3c31b087f1bcb1f753e0db6aca6b
p-Lukas <16989928+p-Lukas@users.noreply.github.com>
2023-10-02 21:21:49 +0200

    progress

---
+++ takahe/EnaiColors.swift
@@ -0,0 +1,9 @@
+//Created on 02.10.23
+
+import Cocoa
+
+struct EnaiColors{
+    static let DefaultBackground = CGColor(srgbRed: 253.0, green: 253.0, blue: 252.0, alpha: 100.0)
+    static let SandLight3 = NSColor(srgbRed: 243.0, green: 243.0, blue: 242.0, alpha: 100.0)
+    static let SandLight12 = NSColor(srgbRed: 27.0, green: 27.0, blue: 24.0, alpha: 100.0)
+}
