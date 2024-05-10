89b5aee69497baf08d9f9625aed5713b6ff2a301
p-Lukas <patrick@enai.io>
2024-05-10 11:58:04 +0200

    feat: over state on niActionImages

--- ni/utils/NiActionImage.swift
+++ ni/utils/NiActionImage.swift
@@ -11,10 +11,44 @@ import Cocoa
 class NiActionImage: NSImageView{
 	
 	var mouseDownFunction: ((NSEvent) -> Void)?
+	var isActiveFunction: (() -> Bool)?
+	private var prevColor: NSColor? = nil
 
+	required init?(coder: NSCoder) {
+		super.init(coder: coder)
+		
+		let hoverEffect = NSTrackingArea.init(rect: self.bounds, options: [.mouseEnteredAndExited, .activeInKeyWindow], owner: self, userInfo: nil)
+		self.addTrackingArea(hoverEffect)
+	}
+	
 	override func mouseDown(with event: NSEvent) {
 		if (mouseDownFunction != nil){
 			mouseDownFunction!(event)
+		}
+	}
+	
+	override func mouseEntered(with event: NSEvent) {
+		//if is not active - don't change color
+		if(isActiveFunction != nil){
+			if(!isActiveFunction!()){
+				return
+			}
+		}
+		prevColor = self.contentTintColor
+		self.contentTintColor = NSColor(.intAerospaceOrange)
+	}
+	
+	override func mouseExited(with event: NSEvent) {
+		//if is not active - don't change color
+		if(isActiveFunction != nil){
+			if(!isActiveFunction!()){
+				return
+			}
+		}
+		if(prevColor != nil){
+			self.contentTintColor = prevColor!
+		}else{
+			self.contentTintColor = NSColor(.sandLight11)
 		}
 	}
 }
