6d99e135e0e5a2ee7d19503e459ca588434b7c03
p-Lukas <patrick@enai.io>
2024-05-27 16:24:44 +0200

    part of ni-17: active/inactive
    All CF content, called CFElements, now has an setActive() setInactive() function

---
+++ ni/niSpace/contentFrame/CFElement.swift
@@ -0,0 +1,13 @@
+//
+//  CFElement.swift
+//  ni
+//
+//  Created by Patrick Lukas on 27/5/24.
+//
+
+import Foundation
+
+protocol CFElement{
+	func setActive() -> Void
+	func setInactive() -> Void
+}
--- ni/niSpace/contentFrame/NiNoteViewElements/NiNoteView.swift
+++ ni/niSpace/contentFrame/NiNoteViewElements/NiNoteView.swift
@@ -7,6 +7,15 @@
 
 import Cocoa
 
-class NiNoteView: NSTextView {
-
+class NiNoteView: NSTextView, CFElement {
+	
+	func setActive() {
+		self.isEditable = true
+		self.isSelectable = true
+	}
+	
+	func setInactive() {
+		self.isEditable = false
+		self.isSelectable = false
+	}
 }
