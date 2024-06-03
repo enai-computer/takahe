162fb68edff76e6986ffd5815f14ea2127d36462
p-Lukas <patrick@enai.io>
2024-06-03 15:17:38 +0200

    ref ni-122 refactoring
    moving TextField params to controller

--- ni/niSpace/contentFrame/NiNoteViewElements/NiNoteUtils.swift
+++ ni/niSpace/contentFrame/NiNoteViewElements/NiNoteUtils.swift
@@ -7,27 +7,9 @@
 
 import Cocoa
 
-func getNewNoteView(owner: ContentFrameController, parentView: NSView, frame: NSRect) -> NiNoteItem{
-	let view = NiNoteItem()//(frame: frame)	//owner: owner,
-	view.owner = owner
-//	
-//	view.textContainerInset = NSSize(width: 6.0, height: 6.0)
-//	view.backgroundColor = NSColor.sandLight3
-//	view.insertionPointColor = NSColor.birkin
-//	view.importsGraphics = false
-//	view.allowsImageEditing = false
-//	view.displaysLinkToolTips = false
-//	view.usesFindBar = false
-//	view.usesFindPanel = false
-//	view.usesFontPanel = false
-//	view.isRichText = false
-//	view.isVerticallyResizable = false
-//	view.isHorizontallyResizable = false
-//	view.isEditable = false
-//	
-//	view.font = NSFont(name: "Sohne-Buch", size: 16.0)
-//	view.textColor = NSColor.sandDark7
-//	
-//	
-	return view
+func getNewNoteItem(owner: ContentFrameController, parentView: NSView, frame: NSRect) -> NiNoteItem{
+	let item = NiNoteItem()//(frame: frame)	//owner: owner,
+	item.owner = owner
+
+	return item
 }
