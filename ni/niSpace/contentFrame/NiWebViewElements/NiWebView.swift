af0f5f03b1a143bbc3071810c1be65e018573e1b
p-Lukas <patrick@enai.io>
2024-06-20 18:14:55 +0200

    completes ni-110

--- ni/niSpace/contentFrame/NiWebViewElements/NiWebView.swift
+++ ni/niSpace/contentFrame/NiWebViewElements/NiWebView.swift
@@ -10,6 +10,49 @@ import Cocoa
 import Carbon.HIToolbox
 import WebKit
 
+class NiWebViewZoomLevels{
+	static let levels: [Int: CGFloat] = [
+		0 : 0.25,
+		1 : 0.33,
+		2 : 0.50,
+		3 : 0.666,
+		4 : 0.75,
+		5 : 0.80,
+		6 : 0.90,
+		7 : 1.00,
+		8 : 1.10,
+		9 : 1.25,
+		10 : 1.50,
+		11 : 1.75,
+		12 : 2.00,
+		13 : 2.50,
+		14 : 3.00,
+		15 : 4.00,
+		16 : 5.00
+	]
+	static let maxLevel = 16
+
+	static func zoomOut(_ currentLevel: Int) -> (Int, CGFloat){
+		if(0 < currentLevel){
+			let nxtLevel = currentLevel - 1
+			return (nxtLevel, levels[nxtLevel]!)
+		}
+		return (currentLevel, levels[currentLevel]!)
+	}
+	
+	static func zoomIn(_ currentLevel: Int) -> (Int, CGFloat){
+		if(currentLevel < maxLevel){
+			let nxtLevel = currentLevel + 1
+			return (nxtLevel, levels[nxtLevel]!)
+		}
+		return (currentLevel, levels[currentLevel]!)
+	}
+	
+	static func defaultZoomLevel() -> (Int, CGFloat){
+		return (7, levels[7]!)
+	}
+}
+
 
 class NiWebView: WKWebView, CFContentItem{
     
@@ -21,10 +64,10 @@ class NiWebView: WKWebView, CFContentItem{
 	// overlays own view to deactivate clicks and visualise deactivation state
 	private var overlay: NSView?
 	
+	var zoomLevel: Int = 7
+	
     init(owner: ContentFrameController, frame: NSRect) {
-        
         self.owner = owner
-		
 		let wvConfig = generateWKWebViewConfiguration()
         
         super.init(frame: frame, configuration: wvConfig)
@@ -128,10 +171,18 @@ class NiWebView: WKWebView, CFContentItem{
 		return
 	}
 	
-//	override func mouseDown(with event: NSEvent) {
-//		print("clicked into WebView")
-//	}
-//	
+
+	@IBAction func zoomIn(_ sender: NSMenuItem) {
+		(zoomLevel, self.pageZoom) = NiWebViewZoomLevels.zoomIn(zoomLevel)
+	}
+	
+	@IBAction func zoomOut(_ sender: NSMenuItem) {
+		(zoomLevel, self.pageZoom) = NiWebViewZoomLevels.zoomOut(zoomLevel)
+	}
+	
+	@IBAction func zoomReset(_ sender: NSMenuItem) {
+		(zoomLevel, self.pageZoom) = NiWebViewZoomLevels.defaultZoomLevel()
+	}
 }
 
 class GlobalScriptMessageHandler: NSObject, WKScriptMessageHandler {
