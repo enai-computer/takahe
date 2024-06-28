514f10130f7da64114cb905476da247004dc38c3
p-Lukas <patrick@enai.io>
2024-06-28 15:16:25 +0200

    updated webView with config

--- ni/niSpace/contentFrame/NiWebViewElements/NiWebViewUtils.swift
+++ ni/niSpace/contentFrame/NiWebViewElements/NiWebViewUtils.swift
@@ -71,6 +71,8 @@ func generateWKWebViewConfiguration() -> WKWebViewConfiguration{
 	wvConfig.applicationNameForUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15"
 	wvConfig.allowsAirPlayForMediaPlayback = true
 	wvConfig.preferences.isElementFullscreenEnabled = true
+	wvConfig.preferences.isFraudulentWebsiteWarningEnabled = true
+	wvConfig.allowsInlinePredictions = true
 	return wvConfig
 }
 
