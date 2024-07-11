a374e56fbf53f944dd96ac09ad345f9bc902f162
p-Lukas <patrick@enai.io>
2024-07-11 13:15:23 +0200

    ref ni-161 updated blur

--- ni/niSpace/SpaceTopbar.swift
+++ ni/niSpace/SpaceTopbar.swift
@@ -7,45 +7,28 @@
 
 import Cocoa
 
-class SpaceTopbar: NSBox{
-	
-	private var contentBlurView: NSView?
-	private var visualEffectsView: NSVisualEffectView?
+class SpaceTopbar: NSView{
 	
 	override func awakeFromNib() {
 		super.awakeFromNib()
-		contentView?.wantsLayer = true
-		contentView?.layer?.cornerRadius = 15.0
-		contentView?.layer?.cornerCurve = .continuous
-		contentView?.layer?.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-		contentView?.layer?.masksToBounds = true
-		contentView?.layer?.backgroundColor = NSColor(.sand1T10).cgColor
+		wantsLayer = true
 		
-		contentBlurView = NSView(frame: self.frame)
-		contentBlurView?.wantsLayer = true
-		setupBlurLayerView(contentBlurView!, inputRadius: 10.0, inputSaturation: 0.0)
-		contentBlurView?.layer?.cornerRadius = 15.0
-		superview!.addSubview(contentBlurView!, positioned: .below, relativeTo: self)
+		layer?.shadowColor = NSColor.sand9.cgColor
+		layer?.shadowRadius = 2.0
+		layer?.shadowOffset = CGSize(width: 0.0, height: 0.0)
+		layer?.shadowOpacity = 1.0
+		
+		layer?.cornerRadius = 10.0
+		layer?.cornerCurve = .continuous
+		layer?.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+		layer?.masksToBounds = true
+		layer?.backgroundColor = NSColor(.sand1T10).cgColor
 	}
 	
 	override func updateTrackingAreas() {
 		super.updateTrackingAreas()
 	}
-	
-	override func resize(withOldSuperviewSize oldSize: NSSize) {
-		super.resize(withOldSuperviewSize: oldSize)
-		contentBlurView?.frame = frame
-		visualEffectsView?.frame = frame
-	}
-	
-	override func mouseEntered(with event: NSEvent) {
-		contentView?.layer?.backgroundColor = NSColor.sand1.cgColor
-	}
-	
-	override func mouseExited(with event: NSEvent) {
-		contentView?.layer?.backgroundColor = NSColor.sand1T10.cgColor
-	}
-	
+
 	//needed as otherwise contentFrames will catch the mouseDown events
 	override func mouseDown(with event: NSEvent) {
 		nextResponder?.mouseDown(with: event)
