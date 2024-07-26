3e2525aecfe677e7c430e9a48da251afe22bb1fc
p-Lukas <patrick@enai.io>
2024-07-26 10:30:54 +0200

    feat: UI for Pinned Apps

--- ni/niSpace/pinnedMenu/NiPinnedMenuViewController.swift
+++ ni/niSpace/pinnedMenu/NiPinnedMenuViewController.swift
@@ -9,10 +9,14 @@ import Cocoa
 
 class NiPinnedMenuViewController: NSViewController{
 	
-	let items: [NSView] //FixME
+	let items: [WebAppItem] //FixME
+	let spaceDocController: NiSpaceDocumentController
+	private let myViewHeight: CGFloat
 	
-	init(items: [NSView]) {
+	init(items: [WebAppItem], docController: NiSpaceDocumentController, height: CGFloat) {
 		self.items = items
+		self.spaceDocController = docController
+		self.myViewHeight = height
 		super.init(nibName: nil, bundle: nil)
 	}
 	
@@ -22,12 +26,46 @@ class NiPinnedMenuViewController: NSViewController{
 	
 	override func loadView() {
 		let myView = (NSView.loadFromNib(nibName: "NiPinnedMenuView", owner: self) as! NiPinnedMenuView)
+		let viewItems = genMenuItemViews(items: self.items)
+		myView.lstOfMenuItems.setViews(viewItems, in: .top)
 		myView.frame.origin = NSPoint(x: 10.0, y: 10.0)
-//		myView.frame.size.height = wSize.height - 20.0
-		view = RoundedRectView(frame: NSRect(origin: CGPoint(x: 0.0, y: 0.0), size: CGSize(width: 68.0, height: 68.0)))
+		myView.frame.size.height = self.myViewHeight
+		view = RoundedRectView(frame: NSRect(origin: CGPoint(x: 0.0, y: 0.0), size: CGSize(width: 68.0, height: self.myViewHeight + 20.0)))
 		view.addSubview(myView)
+	}
+
+	override func viewWillLayout() {
+		super.viewWillLayout()
+	}
+	
+	override func viewDidLayout() {
+		super.viewDidLayout()
 	}
 	
 	//TODO: set up NiActionImages and add to Stack view
+	private func genMenuItemViews(items: [WebAppItem]) -> [NSView]{
+		var menuItems: [NSView] = []
+		
+		for item in items{
+			let itemView = NiActionImage(
+				image: item.icon,
+				with: NSSize(width: 28.0, height: 28.0)
+			)
+			itemView.isActiveFunction = {return true}
+			itemView.setMouseDownFunction(item.openWebApp, with: spaceDocController)
+			menuItems.append(itemView)
+		}
+		
+		return menuItems
+	}
+}
+
+extension WebAppItem{
 	
+	func openWebApp(with event: NSEvent, context: Any){
+		if let docController = context as? NiSpaceDocumentController{
+			let cfController = docController.openEmptyCF(openInitalTab: false)
+			cfController.openWebsiteInNewTab(url.absoluteString, shallSelectTab: true)
+		}
+	}
 }
