95694dfcec344e1caec464abf0c7c42f31d69346
p-Lukas <patrick@enai.io>
2024-07-27 20:58:17 +0200

    connection stroke not drawn correctly

--- ni/niLibrary/NiLibraryConnectionViewElement.swift
+++ ni/niLibrary/NiLibraryConnectionViewElement.swift
@@ -7,24 +7,22 @@
 
 import Cocoa
 
-class NiLibraryConnectionViewElement: NSView{
+@IBDesignable class NiLibraryConnectionViewElement: NSView{
 	
-	@IBInspectable public var showLeftRight: Bool = false
-	//	 {
-	//		didSet {
-	//			updateView()
-	//		}
-	//}
+	@IBInspectable public var lineBottomLeftToTopRight: Bool = false {
+		didSet {
+			needsDisplay = true
+		}
+	}
 	
-	@IBInspectable public var showRightLeft: Bool = false
-//	 {
-//		didSet {
-//			updateView()
-//		}
-//	}
-	
-	@IBOutlet var diagonal: NSImageView!
-	@IBOutlet var diagonalMirrored: NSImageView!
+	@IBInspectable public var lineBottomRightToTopLeft: Bool = false{
+		didSet {
+			needsDisplay = true
+		}
+	}
+	@IBInspectable public var strokeColor: NSColor = .birkin
+	@IBInspectable public var strokeWidth: CGFloat = 2.0
+
 	@IBOutlet var middleDot: NSImageView!
 	
 	required init?(coder: NSCoder) {
@@ -36,23 +34,38 @@ class NiLibraryConnectionViewElement: NSView{
 	
 	override open func awakeFromNib() {
 		super.awakeFromNib()
-		updateView()
+//		updateView()
 	}
 	
-	private func updateView(){
-		if(showLeftRight){
-			diagonal?.isHidden = false
+	override func draw(_ dirtyRect: NSRect) {
+		super.draw(dirtyRect)
+		
+		strokeColor.set() // choose color
+		let figure = NSBezierPath() // container for line(s)
+		
+		if(lineBottomLeftToTopRight){
+			figure.move(to: frame.origin)
+			figure.line(to: NSPoint(x: frame.maxX, y: frame.maxY))
+			
+//			figure.move(to: dirtyRect.origin)
+//			figure.line(to: NSPoint(x: dirtyRect.maxX, y: dirtyRect.maxY))
 		}
-		if(showRightLeft){
-			diagonalMirrored?.isHidden = false
+		if(lineBottomRightToTopLeft){
+			figure.move(to: NSPoint(x: frame.maxX, y: frame.minY))
+			figure.line(to: NSPoint(x: frame.minX, y: frame.maxY))
+//			figure.move(to: NSPoint(x: dirtyRect.maxX, y: dirtyRect.minY))
+//			figure.line(to: NSPoint(x: dirtyRect.minX, y: dirtyRect.maxY))
 		}
+		figure.lineWidth = strokeWidth  // hair line
+		figure.stroke()  // draw line(s) in color
 	}
-//	override func layout() {
-//		if(middleDot != nil){
-//			let xDot = frame.midX - 16.0
-//			let yDot = frame.midY - 16.0
-//			middleDot.frame.origin = CGPoint(x: xDot, y: yDot)
-//		}
-//		super.layout()
-//	}
+	
+	override func layout() {
+		if(middleDot != nil){
+			let xDot = frame.midX - 16.0
+			let yDot = frame.midY - 16.0
+			middleDot.frame.origin = CGPoint(x: xDot, y: yDot)
+		}
+		super.layout()
+	}
 }
