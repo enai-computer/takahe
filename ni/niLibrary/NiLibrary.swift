292c3eba64bc525d313416278f4e1368161ebc0d
p-Lukas <patrick@enai.io>
2024-07-27 17:24:55 +0200

    adding library UI

--- ni/niLibrary/NiLibrary.swift
+++ ni/niLibrary/NiLibrary.swift
@@ -1,25 +1,23 @@
 //
-//  NiPalette.swift
+//  NiLibrary.swift
 //  ni
 //
-//  Created by Patrick Lukas on 12/6/24.
+//  Created by Patrick Lukas on 27/7/24.
 //
 
 import Cocoa
-import Carbon.HIToolbox
 
-class NiPalette: NSPanel, NiSearchWindowProtocol {
+class NiLibrary: NSPanel{
 	
 	private let niDelegate: NiPaletteWindowDelegate
 	override var canBecomeKey: Bool {return true}
 	override var canBecomeMain: Bool {return false}
 	
 	private var windowBlurView: NSView?
-	private var paletteBlurView: NSView?
-	
+
 	init(){
 		let mainWindow = NSApplication.shared.mainWindow!
-		let paletteRect = NiPalette.calcPaletteRect(mainWindow.frame.size)
+		let paletteRect = NiLibrary.calcPaletteRect(mainWindow.frame.size)
 		let frameRect = NSPanel.rectForScreen(paletteRect, screen: mainWindow.screen!)
 		
 		niDelegate = NiPaletteWindowDelegate()
@@ -43,6 +41,25 @@ class NiPalette: NSPanel, NiSearchWindowProtocol {
 		setBlurOnMainWindow(mainWindow)
 	}
 	
+	private func setBlurOnMainWindow(_ mainWindow: NSWindow){
+		windowBlurView = NSView(frame: mainWindow.frame)
+		windowBlurView?.frame.origin = CGPoint(x: 0.0, y: 0.0)
+		windowBlurView!.wantsLayer = true
+		setupBlurOnView(windowBlurView!, inputRadius: 1.0, inputSaturation: 0.6)
+		
+		//needs to happen if the display that is shown on is not main
+		let backAdjustedOriginRect = CGRect(
+			origin: CGPoint(
+				x: self.frame.origin.x - mainWindow.screen!.frame.origin.x,
+				y: self.frame.origin.y - mainWindow.screen!.frame.origin.y
+			),
+			size: self.frame.size)
+		
+		mainWindow.contentView!.addSubview(windowBlurView!)
+	
+		windowBlurView!.layer?.needsDisplay()
+	}
+	
 	private static func calcPaletteRect(_ screenSize: NSSize) -> NSRect{
 		let w = 586.0
 		let h = 426.0
@@ -58,49 +75,5 @@ class NiPalette: NSPanel, NiSearchWindowProtocol {
 			width: w,
 			height: h
 		)
-	}
-	
-	private func setBlurOnMainWindow(_ mainWindow: NSWindow){
-		windowBlurView = NSView(frame: mainWindow.frame)
-		windowBlurView?.frame.origin = CGPoint(x: 0.0, y: 0.0)
-		windowBlurView!.wantsLayer = true
-		setupBlurOnView(windowBlurView!, inputRadius: 1.0, inputSaturation: 0.6)
-		
-		//needs to happen if the display that is shown on is not main
-		let backAdjustedOriginRect = CGRect(
-			origin: CGPoint(
-				x: self.frame.origin.x - mainWindow.screen!.frame.origin.x,
-				y: self.frame.origin.y - mainWindow.screen!.frame.origin.y
-			),
-			size: self.frame.size)
-		paletteBlurView = NSView(frame: backAdjustedOriginRect)
-		paletteBlurView!.wantsLayer = true
-		setupBlurOnView(paletteBlurView!, inputRadius: 8.0, inputSaturation: 1.0)
-		paletteBlurView?.layer?.cornerRadius = 15.0
-		
-		mainWindow.contentView!.addSubview(windowBlurView!)
-		mainWindow.contentView!.addSubview(paletteBlurView!)
-		
-		windowBlurView!.layer?.needsDisplay()
-		paletteBlurView?.layer?.needsDisplay()
-	}
-	
-	override func cancelOperation(_ sender: Any?) {
-		removeSelf()
-	}
-	
-	override func keyDown(with event: NSEvent) {
-		if(event.keyCode == kVK_Escape){
-			removeSelf()
-		}
-	}
-	
-	func removeSelf(){
-		windowBlurView?.removeFromSuperview()
-		windowBlurView = nil
-		paletteBlurView?.removeFromSuperview()
-		paletteBlurView = nil
-		self.orderOut(nil)
-		self.close()
 	}
 }
