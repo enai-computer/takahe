9b48cdeec21690ca4bae030595266a82bfb724c5
p-Lukas <patrick@enai.io>
2024-07-29 13:41:45 +0200

    ref ni-182 loading animation

---
+++ ni/niLibrary/LoadingView.swift
@@ -0,0 +1,13 @@
+//
+//  LoadingView.swift
+//  ni
+//
+//  Created by Patrick Lukas on 29/7/24.
+//
+
+import Cocoa
+
+class LoadingView: NSView{
+	
+	@IBOutlet var loadingAnimation: NSImageView!
+}
