d7ef42eea022a2419526174a30fbcb7e59432231
p-Lukas <patrick@enai.io>
2024-08-29 13:49:11 +0200

    ref: ni-200

--- ni/niSpace/NiSpaceViewController.swift
+++ ni/niSpace/NiSpaceViewController.swift
@@ -239,7 +239,6 @@ class NiSpaceViewController: NSViewController, NSTextFieldDelegate{
 	}
 	
 	func removePinnedWebApp(_ model: NiPinnedWebAppVModel){
-		model.webView = nil
 		self.webApps.removeAll(where: {$0 == model})
 		UserSettings.removeValue(setting: .pinnedWebApps, value: model.itemData)
 	}
