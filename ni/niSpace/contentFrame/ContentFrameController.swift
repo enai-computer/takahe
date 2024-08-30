enai-computer/takahe

commit 4d0271a08281ac1856cd8479c8e710cf874fa8a3

should fix crash report from 28th of Aug

ni/niSpace/contentFrame/ContentFrameController.swift
@@ -1215,9 +1215,13 @@ class ContentFrameController: NSViewController, WKNavigationDelegate, WKUIDelega
 		}
 		guard viewHasTabs() else {return}
 		
-		if(0 < webView.tabHeadPosition && webView.tabHeadPosition < tabs.count){
+		if(0 <= webView.tabHeadPosition && webView.tabHeadPosition < tabs.count){
 			self.tabs[webView.tabHeadPosition].title = webView.getTitle() ?? tabs[webView.tabHeadPosition].title
-			viewWithTabs?.cfTabHeadCollection?.reloadItems(at: Set(arrayLiteral: IndexPath(item: webView.tabHeadPosition, section: 0)))
+			if let nrOfItems: Int = viewWithTabs?.cfTabHeadCollection?.numberOfItems(inSection: 0){
+				if(webView.tabHeadPosition < nrOfItems){
+					viewWithTabs?.cfTabHeadCollection?.reloadItems(at: Set(arrayLiteral: IndexPath(item: webView.tabHeadPosition, section: 0)))
+				}
+			}
 		}
 	}
 	

commit 0609b1c7297007be7dd355e1e5bb88c5e68738df

fixes bug introduced in: https://github.com/enai-io/takahe/commit/4d0271a08281ac1856cd8479c8e710cf874fa8a3

ni/niSpace/contentFrame/ContentFrameController.swift
@@ -845,7 +845,7 @@ class ContentFrameController: NSViewController, WKNavigationDelegate, WKUIDelega
 	 Close tabs and select the nxt tab to the right first and then left when none to the right are left
 	 */
 	func closeSelectedTab(){
-		if(0 < tabs.count && tabs[0].type == .web){
+		if(0 < tabs.count && tabs[0].type == .web && viewState == .simpleFrame){
 			confirmClose()
 			return
 		}