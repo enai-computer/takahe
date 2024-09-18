518b441a0b831dcce328f03427fd3e064ed3b8a1
p-Lukas <patrick@enai.io>
2024-09-18 14:01:57 +0200

    ref: ni-208

---
+++ ni/models/BilgeWater.swift
@@ -0,0 +1,43 @@
+//
+//  BilgeWater.swift
+//  ni
+//
+//  Created by Patrick Lukas on 18/9/24.
+//
+
+import Foundation
+
+/// https://en.wikipedia.org/wiki/Bilge#Bilge_water
+struct BilgeWater: Codable{
+	var appStats: WaterAppStats?
+	var systemStats: WaterSystemStats?
+	
+	var openSpaceAsPersistedOnDisk: NiDocumentModel?
+	var openSpaceAsInMemory: NiDocumentModel?
+}
+
+
+struct WaterAppStats: Codable{
+	//app version
+	var releaseVersionNumer: String?
+	var buildVersionNumber: String?
+	
+	//App stats
+	var memoryUsedInKBytes: String?
+	var threadCount: String?
+}
+
+struct WaterSystemStats: Codable{
+	var osVersion: String?
+	
+	var activeProcessorCount: Int?
+	var processorCount: Int?
+	var isLowerPowerModeEnabled: Bool?
+	
+	var totalMemoryInGB: String?
+	var availableMemoryInMB: String?
+	
+	var systemUptime: String?
+	
+	var availableNetworks: String
+}
