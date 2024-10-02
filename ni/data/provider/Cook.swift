50c9ea60536f56f2d142d6443496dd6614d8c7d5
p-Lukas <patrick@enai.io>
2024-10-02 09:19:26 +0200

    fix: ni-236 result ordering
    spaces always on top

--- ni/data/provider/Cook.swift
+++ ni/data/provider/Cook.swift
@@ -59,13 +59,9 @@ class Cook{
 		
 		//MARK: sorting
 		if(giveCreateNewSpaceOption && typedChars != nil && !typedChars!.isEmpty){
-			//FIXME: hacky sorting solution
-			res = res.sorted{
-				if($1.name.lowercased().starts(with: typedChars!.lowercased())){
-					return false
-				}
-				return true
-			}
+			let searchStr = typedChars!.lowercased()
+			res = res.sorted{compareResult(r0: $0, r1: $1, searchStr: searchStr)}
+			
 			res.append(NiSearchResultItem(type: .niSpace, id: NiSpaceDocumentController.EMPTY_SPACE_ID, name: "Create a new space", data: nil))
 			
 			let wordCount = countWords(in: typedChars!)
@@ -82,6 +78,19 @@ class Cook{
 			}
 		}
 		return res
+	}
+	
+	private func compareResult(r0: NiSearchResultItem, r1: NiSearchResultItem, searchStr: String) -> Bool{
+		if(r0.type == .niSpace && r1.type != .niSpace){
+			return true
+		}
+		if(r1.type == .niSpace && r0.type != .niSpace){
+			return false
+		}
+		if(r1.name.lowercased().starts(with: searchStr)){
+			return false
+		}
+		return true
 	}
 	
 	private func searchSpaces(typedChars: String?,
