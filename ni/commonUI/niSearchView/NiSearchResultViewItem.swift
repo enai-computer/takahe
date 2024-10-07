enai-computer/takahe

commit 1a2303101cb69211079296decb6a9fc0892fefe7

completes: ni-236
jumps directly to documents in different spaces
search params will need to be defined
ready for testing

ni/commonUI/niSearchView/NiSearchResultViewItem.swift
@@ -65,8 +65,8 @@ class NiSearchResultViewItem: NSCollectionViewItem {
 		}
 		
 		if(data.type == .pdf || data.type == .web || data.type == .group){
-			if let parentSpaceName: String = data.data as? String{
-				resultSubTitle.stringValue = parentSpaceName
+			if let parentSpace: NiSRIOriginData = data.data as? NiSRIOriginData{
+				resultSubTitle.stringValue = parentSpace.name
 			}
 		}
 	}
@@ -109,27 +109,61 @@ class NiSearchResultViewItem: NSCollectionViewItem {
 	
 	func tryOpenResult(){
 		if(resultData?.type == .niSpace){
-			openSpaceAndTryRemoveWindow()
+			guard  let spaceId = resultData?.id else {return}
+			openSpaceAndTryRemoveWindow(for: spaceId, with: resultTitle.stringValue)
 		}else if(resultData?.type == .eve){
 			niSearchController?.getAnswerFromEve()
+		}else if(resultData?.type == .group){
+			guard let spaceToOpen = self.resultData?.data as? NiSRIOriginData else {return}
+			openSpaceAndTryRemoveWindow(groupItem: self.resultData!, in: spaceToOpen)
+		}else if(resultData?.type == .pdf || resultData?.type == .web){
+			guard let spaceToOpen = self.resultData?.data as? NiSRIOriginData else {return}
+			openSpaceAndTryRemoveWindow(contentItem: self.resultData!, in: spaceToOpen)
 		}
 	}
 	
-	private func openSpaceAndTryRemoveWindow(){
-		if(resultData?.id == nil){return}
-		if let spaceViewController = NSApplication.shared.mainWindow?.contentViewController as? NiSpaceViewController{
-			spaceViewController.showHeader()
-			if(resultData?.id == NiSpaceDocumentController.EMPTY_SPACE_ID){
-				if let spaceName = getEnteredSearchText()?.trimmingCharacters(in: .whitespaces){
-					spaceViewController.createSpace(name: spaceName)
-				}
-			}else{
-				spaceViewController.loadSpace(spaceId: resultData!.id!, name: resultTitle.stringValue)
+	private func openSpaceAndTryRemoveWindow(for id: UUID, with name: String){
+		guard let spaceViewController = NSApplication.shared.mainWindow?.contentViewController as? NiSpaceViewController else {return}
+		
+		if(spaceViewController.niSpaceID == id){
+			if let paletteWindow = view.window as? NiSearchWindowProtocol{
+				paletteWindow.removeSelf()
 			}
+			return
 		}
+		
+		spaceViewController.showHeader()
+		if(id == NiSpaceDocumentController.EMPTY_SPACE_ID){
+			if let spaceName = getEnteredSearchText()?.trimmingCharacters(in: .whitespaces){
+				spaceViewController.createSpace(name: spaceName)
+			}
+		}else{
+			spaceViewController.loadSpace(spaceId: id, name: name)
+		}
+		
 		if let paletteWindow = view.window as? NiSearchWindowProtocol{
 			paletteWindow.removeSelf()
 		}
+	}
+	
+	private func openSpaceAndTryRemoveWindow(
+		groupItem: NiSearchResultItem,
+		in spaceOrigin: NiSRIOriginData
+	){
+		openSpaceAndTryRemoveWindow(for: spaceOrigin.id, with: spaceOrigin.name)
+		guard let spaceViewController = NSApplication.shared.mainWindow?.contentViewController as? NiSpaceViewController else {return}
+		guard let groupId = groupItem.id else {return}
+		spaceViewController.niDocument.myView.highlightContentFrame(with: groupId)
+	}
+	
+	private func openSpaceAndTryRemoveWindow(
+		contentItem: NiSearchResultItem,
+		in spaceOrigin: NiSRIOriginData
+	){
+		openSpaceAndTryRemoveWindow(for: spaceOrigin.id, with: spaceOrigin.name)
+		guard let spaceViewController = NSApplication.shared.mainWindow?.contentViewController as? NiSpaceViewController else {return}
+		guard let contentId = contentItem.id else {return}
+		spaceViewController.niDocument.myView.highlightContentObj(contentId: contentId)
 	}
 	
 	private func openImmersiveView(){

ni/data/provider/Cook.swift
@@ -20,6 +20,11 @@ struct NiSearchResultItem{
 	let data: Any?
 }
 
+struct NiSRIOriginData{
+	let id: UUID
+	let name: String
+}
+
 
 /** Named after Captain James Cook
  
@@ -157,11 +162,13 @@ class Cook{
 							type: type,
 							id: try record.get(ContentTable.table[ContentTable.id]),
 							name: try record.get(ContentTable.table[ContentTable.title]) ?? "",
-							data: try record.get(DocumentTable.table[DocumentTable.name])
+							data: NiSRIOriginData(
+								id: try record.get(DocumentTable.table[DocumentTable.id]),
+								name: try record.get(DocumentTable.table[DocumentTable.name])
+							)
 						)
 					)
 				}
-
 			}
 		}catch{
 			print(error)
@@ -175,6 +182,7 @@ class Cook{
 			ContentTable.table[ContentTable.id],
 			ContentTable.table[ContentTable.title],
 			ContentTable.table[ContentTable.type],
+			DocumentTable.table[DocumentTable.id],
 			DocumentTable.table[DocumentTable.name]
 		)
 		.join(
@@ -218,7 +226,10 @@ class Cook{
 						type: .group,
 						id: try record.get(GroupTable.table[GroupTable.id]),
 						name: try record.get(GroupTable.table[GroupTable.name]),
-						data: try record.get(DocumentTable.table[DocumentTable.name])
+						data: NiSRIOriginData(
+							id: try record.get(DocumentTable.table[DocumentTable.id]),
+							name: try record.get(DocumentTable.table[DocumentTable.name])
+						)
 					)
 				)
 			}
@@ -232,6 +243,7 @@ class Cook{
 		return GroupTable.table.select(
 			GroupTable.table[GroupTable.id],
 			GroupTable.table[GroupTable.name],
+			DocumentTable.table[DocumentTable.id],
 			DocumentTable.table[DocumentTable.name]
 		)
 		.join(

ni/niSpace/NiSpaceDocumentView.swift
@@ -163,8 +163,35 @@ class NiSpaceDocumentView: NSView{
 			return
 		}
 		
-		setTopNiFrame(orderedCFs[nxtContentFrame])
-		
+		highlightContentFrame(cframe: orderedCFs[nxtContentFrame])
+	}
+	
+	func highlightContentFrame(with id: UUID){
+		for cFrame in contentFrameControllers{
+			if(cFrame.groupId == id){
+				highlightContentFrame(cframe: cFrame)
+				return
+			}
+		}
+	}
+	
+	func highlightContentObj(contentId: UUID){
+		for cFrame in contentFrameControllers{
+			for tab in cFrame.tabs {
+				if(tab.contentId == contentId){
+					highlightContentFrame(cframe: cFrame)
+					cFrame.selectTab(at: tab.position)
+					if(cFrame.viewState.isMinimized()){
+						cFrame.minimizedToExpanded()
+					}
+					return
+				}
+			}
+		}
+	}
+	
+	private func highlightContentFrame(cframe: ContentFrameController){
+		setTopNiFrame(cframe)
 		if(!NSPointInRect(topNiFrame!.view.frame.origin, visibleRect)){
 			var scrollToPoint = topNiFrame!.view.frame.origin
 			if(50.0 < scrollToPoint.y){

ni/niSpace/contentFrame/ContentFrameController.swift
@@ -37,7 +37,7 @@ class ContentFrameController: NSViewController, WKNavigationDelegate, WKUIDelega
 	private(set) var closeTriggered = false
 	
 	private var groupName: String?
-	private var groupId: UUID?
+	private(set) var groupId: UUID?
 	private var prevDisplayState: NiPreviousDisplayState?
 	
 	/*

commit 82ff9202145799435edafb2025cd500fc2de4a95

hotfix - new space creation
new space creation fails due to a bug introduced in: 1a2303101cb69211079296decb6a9fc0892fefe7

ni.xcodeproj/project.pbxproj
@@ -1520,7 +1520,7 @@
 				"CODE_SIGN_IDENTITY[sdk=macosx*]" = "Apple Development";
 				CODE_SIGN_STYLE = Automatic;
 				COMBINE_HIDPI_IMAGES = YES;
-				CURRENT_PROJECT_VERSION = "0.2.0 --- 2024-09-20.1";
+				CURRENT_PROJECT_VERSION = "0.2.0 --- 2024-10-07.1";
 				DEAD_CODE_STRIPPING = YES;
 				DEVELOPMENT_TEAM = SNXQ3BDQ5V;
 				ENABLE_HARDENED_RUNTIME = YES;
@@ -1553,7 +1553,7 @@
 				CODE_SIGN_IDENTITY = "Apple Development";
 				CODE_SIGN_STYLE = Automatic;
 				COMBINE_HIDPI_IMAGES = YES;
-				CURRENT_PROJECT_VERSION = "0.2.0 --- 2024-09-20.1";
+				CURRENT_PROJECT_VERSION = "0.2.0 --- 2024-10-07.1";
 				DEAD_CODE_STRIPPING = YES;
 				ENABLE_HARDENED_RUNTIME = YES;
 				GENERATE_INFOPLIST_FILE = YES;

ni/commonUI/niSearchView/NiSearchResultViewItem.swift
@@ -125,7 +125,7 @@ class NiSearchResultViewItem: NSCollectionViewItem {
 	private func openSpaceAndTryRemoveWindow(for id: UUID, with name: String){
 		guard let spaceViewController = NSApplication.shared.mainWindow?.contentViewController as? NiSpaceViewController else {return}
 		
-		if(spaceViewController.niSpaceID == id){
+		if(spaceViewController.niSpaceID == id && id != NiSpaceDocumentController.EMPTY_SPACE_ID){
 			if let paletteWindow = view.window as? NiSearchWindowProtocol{
 				paletteWindow.removeSelf()
 			}