efa08272b5cbd392390ab854ba25b0c89d522db4
p-Lukas <patrick@enai.io>
2024-10-14 15:34:04 +0200

    ref: ni-266 reAuth after token expires

--- ni/data/client/MaraeClient.swift
+++ ni/data/client/MaraeClient.swift
@@ -12,11 +12,14 @@ class MaraeClient{
 //	private let hostUrl = "https://enai.host"
 	private let hostUrl = "http://127.0.0.1:8000"
 	private let apiVersion = "/v1"
-	private let verify = "/test-verify"
+	private let verify = "/verify"
 	private let userID = "/00000000-0000-0000-0000-000000000003"
 	
 	private let client: APIClient
+	private let AUTH_HEADER_KEY = "Authorization"
 	private var auth_header: [String: String] = [:]
+	private var updatedAuthToken: Date?
+	private let minTimeBetweenAuth_sec: Double = -60.0
 	
 	init(){
 		client = APIClient(baseURL: URL(string: hostUrl))
@@ -38,7 +41,8 @@ class MaraeClient{
 				if(res.statusCode == 200){
 					let jsonD = JSONDecoder()
 					let body = try jsonD.decode(MaraeVerifyResponse.self, from: res.data)
-					self.auth_header = ["Authorization": "Bearer " + body.access_token]
+					self.auth_header = [self.AUTH_HEADER_KEY: "Bearer " + body.access_token]
+					self.updatedAuthToken = .now
 				}
 			}
 		}
@@ -66,15 +70,28 @@ class MaraeClient{
 		return try await sendRequestNoResponse(delRequest)
 	}
 	
+	func getVersion() async throws -> String{
+		let req = Request(
+			path: "/version",
+			method: .get,
+			headers: auth_header
+		)
+		let res = try await sendRequest(req)
+		if(res?.statusCode == 200 && res != nil){
+			return String(data: res!.data, encoding: .utf8) ?? "failed to decode"
+		}
+		return "Failed to fetch status"
+	}
+	
 	func testStatus() async throws -> String{
 		let req = Request(
 			path: "/status",
 			method: .get,
 			headers: auth_header
 		)
-		let res = try await client.send(req)
-		if(res.statusCode == 200){
-			return String(data: res.data, encoding: .utf8) ?? "failed to decode"
+		let res = try await sendRequest(req)
+		if(res?.statusCode == 200 && res != nil){
+			return String(data: res!.data, encoding: .utf8) ?? "failed to decode"
 		}
 		return "Failed to fetch status"
 	}
@@ -88,10 +105,10 @@ class MaraeClient{
 				("q", question.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")
 			]
 		)
-		let res = try await client.send(req)
-		if(res.statusCode == 200){
+		let res = try await sendRequest(req)
+		if(res?.statusCode == 200 && res != nil){
 			let jsonD = JSONDecoder()
-			let answer = try jsonD.decode(EveChatResponseMessage.self, from: res.data)
+			let answer = try jsonD.decode(EveChatResponseMessage.self, from: res!.data)
 			return answer.message
 		}
 		return "Failed to get answer to your question."
@@ -105,6 +122,26 @@ class MaraeClient{
 		return false
 	}
 	
+	private func sendRequest(_ req: Request<()>) async throws -> Response<Void>?{
+		do{
+			let res = try await client.send(req)
+			return res
+		}catch{
+			if let errorCode = (error as? APIError)?.errorCode as? Int{
+				if(errorCode == 401 && (updatedAuthToken == nil || updatedAuthToken!.timeIntervalSinceNow < minTimeBetweenAuth_sec)){
+					verifyDevice()
+					try await Task.sleep(for: .seconds(3))
+					var reqUpdatedHeader = req
+					reqUpdatedHeader.headers?.removeValue(forKey: AUTH_HEADER_KEY)
+					reqUpdatedHeader.headers?[AUTH_HEADER_KEY] = auth_header[AUTH_HEADER_KEY]
+					return try await sendRequest(reqUpdatedHeader)
+				}
+			}
+			print(error)
+		}
+		return nil
+	}
+	
 	private func getDeviceToken(completion: @escaping (String?) -> ()) {
 		let currentDevice = DCDevice.current
 		guard currentDevice.isSupported else { print("Device is not supported");return}
@@ -114,6 +151,15 @@ class MaraeClient{
 			let tokenString = tokenData.base64EncodedString()
 			print("Received device token")
 			completion(tokenString)
+		}
+	}
+}
+
+extension APIError{
+	public var errorCode: Int? {
+		switch self {
+		case .unacceptableStatusCode(let statusCode):
+				return statusCode
 		}
 	}
 }
