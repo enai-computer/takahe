e58d989a454f8eb35791c5102e6802139f9b2f72
Christian Tietze <me@christiantietze.de>
2024-11-05 10:45:56 +0100

    NI-233: Express content frame state change via transition 'tables' (#37)
    * use pattern matching in minimizeSelf() to highlight valid combinations
    
    * use pattern matching in maximizeSelf() to highlight valid values
    
    * add fullscreenToPreviousState()

--- ni/niSpace/contentFrame/ContentFrameController.swift
+++ ni/niSpace/contentFrame/ContentFrameController.swift
@@ -422,15 +422,17 @@ class ContentFrameController: NSViewController, WKNavigationDelegate, WKUIDelega
 	 * MARK: transitions between views
 	 */
 	func minimizeSelf(){
-		if(viewState == .expanded && (prevDisplayState == nil || prevDisplayState?.state == .minimised)){
+		switch (viewState, prevDisplayState?.state) {
+		case (.expanded, .minimised),
+			 (.expanded, nil):
 			minimizeSelfToDefault()
-		}else if(viewState == .expanded
-				 && prevDisplayState != nil
-				 && prevDisplayState!.state == .collapsedMinimised){
+		case (.expanded, .collapsedMinimised):
 			minimizeToCollapsed()
-		}
-		else if(viewState == .simpleFrame){
+		case (.simpleFrame, _):
 			minimizeSelfToSimple()
+		default:
+			assertionFailure("Unhandled combination of current and previous view state")
+			break
 		}
 	}
 	
@@ -568,7 +570,32 @@ class ContentFrameController: NSViewController, WKNavigationDelegate, WKUIDelega
 		
 		sharedLoadViewSetters()
 	}
-	
+
+	func fullscreenToPreviousState() {
+		assert(self.viewState == .fullscreen)
+		let previousState = prevDisplayState?.state ?? .expanded
+
+		// Minimizing is only supported from expanded state, so transition to expanded first.
+		fullscreenToExpanded()
+
+		switch previousState {
+		case .expanded:
+			break // Already handled by default `fullscreenToExpanded`
+		case .fullscreen:
+			break // Keep expanded view if we don't know any non-fullscreen state
+
+		case .collapsedMinimised:
+			minimizeToCollapsed()
+		case .minimised:
+			minimizeSelfToDefault()
+		case .simpleFrame,
+			 .simpleMinimised:
+			minimizeSelfToSimple()
+		case .frameless:
+			assertionFailure("Frameless views should never have been in fullscreen mode")
+		}
+	}
+
 	func fullscreenToExpanded(){
 		var reloadTabHeads = false
 		if(expandedCFView == nil){
@@ -617,10 +644,13 @@ class ContentFrameController: NSViewController, WKNavigationDelegate, WKUIDelega
 	}
 	
 	func maximizeSelf(){
-		if(viewState == .minimised || viewState == .collapsedMinimised){
+		switch viewState {
+		case .minimised, .collapsedMinimised:
 			minimizedToExpanded()
-		}else if(viewState == .simpleMinimised){
+		case .simpleMinimised:
 			simpleMinimizedToSimpleFrame()
+		case .expanded, .frameless, .simpleFrame, .fullscreen:
+			assertionFailure("Unhandled view state to self-maximize")
 		}
 	}
 	
