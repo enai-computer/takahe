4ec77c17c4ad796f05caffe27edcec1d57c348b2
Christian Tietze <me@christiantietze.de>
2024-11-05 13:19:30 +0100

    add NiMenuItemViewModel.Label enum with old title string variant and view array

--- ni/commonUI/niMenu/NiMenuItemViewModel.swift
+++ ni/commonUI/niMenu/NiMenuItemViewModel.swift
@@ -8,10 +8,35 @@
 import Cocoa
 
 struct NiMenuItemViewModel{
-	
-	let title: String
+	enum Label {
+		case title(String)
+		case views([NSView])
+	}
+
+	let label: Label
+	var title: String {
+		return switch label {
+		case .title(let string): string
+		case .views: ""
+		}
+	}
 	let isEnabled: Bool
 	let mouseDownFunction: ((NSEvent) -> Void)?
 	var keyboardShortcut: String? = nil
-	
+}
+
+extension NiMenuItemViewModel {
+	init(
+		title: String,
+		isEnabled: Bool,
+		mouseDownFunction: ((NSEvent) -> Void)?,
+		keyboardShortcut: String? = nil
+	) {
+		self.init(
+			label: .title(title),
+			isEnabled: isEnabled,
+			mouseDownFunction: mouseDownFunction,
+			keyboardShortcut: keyboardShortcut
+		)
+	}
 }
