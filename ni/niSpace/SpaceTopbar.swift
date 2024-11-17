634e5c405682a1bb1d9ef52dbba940aa7b663c96
p-Lukas <patrick@enai.io>
2024-11-17 17:48:37 +0100

    fixes: click-though in space topbar
    Somehow the webview is smuggled into the responder chain where it shouldn't be. Thus we manually pass the click to the NiSpaceViewController.

--- ni/niSpace/SpaceTopbar.swift
+++ ni/niSpace/SpaceTopbar.swift
@@ -45,6 +45,10 @@ class SpaceTopbar: NSView{
 
 	//needed as otherwise contentFrames will catch the mouseDown events
 	override func mouseDown(with event: NSEvent) {
-		nextResponder?.mouseDown(with: event)
+		if let spaceController = nextResponder?.nextResponder?.nextResponder as? NiSpaceViewController{
+			spaceController.mouseDown(with: event)
+			return
+		}
+		super.mouseDown(with: event)
 	}
 }
