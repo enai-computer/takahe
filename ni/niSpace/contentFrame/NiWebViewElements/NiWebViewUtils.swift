41eb82af083bc28a147b6d2cc27963d8e432e502
p-Lukas <patrick@enai.io>
2024-11-22 14:47:46 +0100

    update user agent string

--- ni/niSpace/contentFrame/NiWebViewElements/NiWebViewUtils.swift
+++ ni/niSpace/contentFrame/NiWebViewElements/NiWebViewUtils.swift
@@ -86,7 +86,7 @@ func generateWKWebViewConfiguration() -> WKWebViewConfiguration{
 	
 	let wvConfig = WKWebViewConfiguration()
 	wvConfig.upgradeKnownHostsToHTTPS = true
-	wvConfig.applicationNameForUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15"
+	wvConfig.applicationNameForUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.1.1 Safari/605.1.15"
 	wvConfig.allowsAirPlayForMediaPlayback = true
 	wvConfig.preferences.isElementFullscreenEnabled = true
 	wvConfig.preferences.isFraudulentWebsiteWarningEnabled = true
