69c95c3413ead6b7cedf95fcd6399ddd7bb24c48
p-Lukas <patrick@enai.io>
2024-11-29 17:58:49 +0100

    positioning left side view 0

--- ni/niOnboarding/steps/Step1ViewLeft.swift
+++ ni/niOnboarding/steps/Step1ViewLeft.swift
@@ -10,28 +10,33 @@ import SwiftUI
 struct Step1ViewLeft: View {
 	
 	var body: some View {
-		HStack(alignment: .center, spacing: 20.0){
-			Spacer()
-			VStack(alignment: .leading, spacing: 20.0){
+		GeometryReader { geometry in
+			HStack(alignment: .center, spacing: 20.0){
 				Spacer()
-				Text("Welcome")
-					.font(.custom("Sohne-Kraftig", size: 30))
-					.foregroundColor(.sand12)
-				
-				Text("Enai is a personal \ninternet computer.")
-					.font(.custom("Sohne-Buch", size: 21))
-					.foregroundColor(.sand12)
-				
-				Text("To simplify your life and \ncare for your attention.")
-					.font(.custom("Sohne-Buch", size: 21))
-					.foregroundColor(.sand12)
+//					.frame(width: geometry.size.width * 0.2)
+				VStack(alignment: .leading, spacing: 20.0){
+					Spacer()
+						.frame(height: geometry.size.height * 0.4)
+					Text("Welcome")
+						.font(.custom("Sohne-Kraftig", size: 30))
+						.foregroundColor(.sand12)
+					
+					Text("Enai is a personal \ninternet computer.")
+						.font(.custom("Sohne-Buch", size: 21))
+						.foregroundColor(.sand12)
+					
+					Text("To simplify your life and \ncare for your attention.")
+						.font(.custom("Sohne-Buch", size: 21))
+						.foregroundColor(.sand12)
+					Spacer()
+				}
 				Spacer()
 			}
-			Spacer()
 		}
 	}
 }
 
 #Preview {
 	Step1ViewLeft()
+		.frame(width: 600, height: 700)
 }
