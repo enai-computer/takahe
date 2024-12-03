d75d683097949af57b66ab3d0db83bab9e0e21ca
p-Lukas <patrick@enai.io>
2024-12-03 12:45:18 +0100

    added baseline step4

--- ni/niOnboarding/steps/Step4ViewLeft.swift
+++ ni/niOnboarding/steps/Step4ViewLeft.swift
@@ -17,17 +17,14 @@ struct Step4ViewLeft: View {
 					Spacer()
 						.frame(height: geometry.size.height * 0.4)
 					
-					Text("A space is a canvas")
-						.font(.custom("Sohne-Kraftig", size: 30))
+					Text("Switch context effortlessly")
+						.font(.custom("Sohne-Buch", size: 30))
 						.foregroundColor(.sand12)
 					
-					Text("For all your info - websites, apps, \nnotes, documents, images...")
+					Text("And less frequently. Keeping \neverything related to each project in \nits own space minimizes your risk of distractions \nto support your attention.")
 						.font(.custom("Sohne-Buch", size: 21))
-						.foregroundColor(.sand12)
-					
-					Text("And an AI personal assistant")
-						.font(.custom("Sohne-Buch", size: 21))
-						.foregroundColor(.sand12)
+						.foregroundColor(.sand115)
+
 					Spacer()
 				}
 				Spacer()
---
+++ ni/niOnboarding/steps/Step4ViewRight.swift
@@ -0,0 +1,33 @@
+//
+//  Step4ViewRight.swift
+//  Enai
+//
+//  Created by Patrick Lukas on 28/11/24.
+//
+
+import SwiftUI
+
+struct Step4ViewRight: View {
+	
+	var body: some View {
+		GeometryReader { geometry in
+			ZStack(alignment: .center) {
+				VStack(alignment: .center){
+					Spacer()
+						.frame(height: geometry.size.height * 0.2)
+					Image("step4RightSideImg")
+						.resizable()
+						.aspectRatio(nil, contentMode: .fit)
+						.position(x: geometry.size.width / 2, y: geometry.size.height / 3)
+					Spacer()
+						.frame(height: geometry.size.height * 0.2)
+				}
+			}
+		}
+	}
+}
+
+#Preview {
+	Step4ViewRight()
+		.frame(width: 900, height: 700)
+}
